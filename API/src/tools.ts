53011fb00a1fd1502dc64bfeff7ba9eb03b1dffb
yseguin <youvataque@icloud.com>
2025-01-09 17:17:21 +0100

    useless code

--- API/src/tools.ts
+++ API/src/tools.ts
@@ -150,35 +150,9 @@ function calculateWordSimilarity2(name: string, comparedName: string): number {
 			matchScore += weight;
 		}
 	});
-	const titleSimilarity = calculateLevenshtein(cleanName(name), cleanName(comparedName));
+	const titleSimilarity = calculateWordSimilarity(cleanName(name), cleanName(comparedName));
 	const finalScore = (matchScore / totalScore) * 100;
 	return 0.7 * finalScore + 0.3 * titleSimilarity;
-}
-
-/////////////////////////////////////////////////////////////////////////////////
-// Distance de Levenshtein pour complément de similarité
-function calculateLevenshtein(a: string, b: string): number {
-	const matrix = Array.from({ length: a.length + 1 }, () =>
-		Array(b.length + 1).fill(0)
-	);
-
-	for (let i = 0; i <= a.length; i++) matrix[i][0] = i;
-	for (let j = 0; j <= b.length; j++) matrix[0][j] = j;
-
-	for (let i = 1; i <= a.length; i++) {
-		for (let j = 1; j <= b.length; j++) {
-			const cost = a[i - 1] === b[j - 1] ? 0 : 1;
-			matrix[i][j] = Math.min(
-				matrix[i - 1][j] + 1,
-				matrix[i][j - 1] + 1,
-				matrix[i - 1][j - 1] + cost
-			);
-		}
-	}
-
-	const distance = matrix[a.length][b.length];
-	const maxLength = Math.max(a.length, b.length);
-	return ((maxLength - distance) / maxLength) * 100;
 }
 
 /////////////////////////////////////////////////////////////////////////////////
